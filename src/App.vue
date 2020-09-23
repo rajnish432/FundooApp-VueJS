0156599fda4a3f1e58e2165d0f041817332ecb68
rajnish432 <rajnish.kahar1996@gmail.com>
2020-09-23 19:19:56 +0530

    [Rajnish]ADD: Welcome To Fundoo App

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>WELCOME TO FUNDOO APP</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  border: 1px solid;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
